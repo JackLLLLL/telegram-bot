2af3c5f33756b53cd05c272d614ea996cc9c1f75
Jack Liang <jiakangl@sfu.ca>
2019-11-19 16:42:23 -0800

    test: add console

--- dota.js
+++ dota.js
@@ -126,7 +126,7 @@ const dotaBot = (bot) => {
                         request.get(`http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?key=632B287DF9E30A8C675CDA16AB3E51D2&steamids=${steamID}`, (err2, res2, body2) => {
                             if (res2.statusCode === 200) {
                                 const summary = JSON.parse(body2).response.players[0];
-
+                                console.log(summary, JSON.parse(body2)
                                 const reply = createOnlineStatusReply(profile.personaname, summary.personastate, summary.lastlogoff, summary.gameextrainfo);
                                 
                                 ctx.reply(reply);
